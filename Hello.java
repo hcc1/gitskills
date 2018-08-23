0d28f8ebcb258bca4cd99efa4ffc8755dd27e39b
hcc1 <amwdny@126.com>
2018-08-23 14:47:11 +0800

    firt java test

---
+++ Hello.java
@@ -0,0 +1,33 @@
+/*hello this is test branch 
+
+second  commit
+
+
+<<<<<<< HEAD
+this is the master commit
+
+=======
+dev branch commit
+>>>>>>> dev
+
+amwdny@126.com  commint
+<<<<<<< HEAD
+i am gohome 
+=======
+this is 9-16 dev add content
+>>>>>>> dev
+
+create a new branch is quick and simple
+
+teist fenzhicelueguangli*/
+
+public class Hello
+{
+	public static void main(String[] args)
+	{
+		System.out.println("hellow teste jenkins")
+	}
+
+
+}
+
